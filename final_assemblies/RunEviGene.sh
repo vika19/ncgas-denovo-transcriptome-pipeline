e89d1945a8e242bab06eae0b3340123ddaa9ab2d
Sheri Sanders <ss93@h2.carbonate.uits.iu.edu>
2018-07-06 11:54:10 -0400

    Initializing cont'd

---
+++ final_assemblies/RunEviGene.sh
@@ -0,0 +1,20 @@
+#PBS -k oe
+#PBS -m abe
+#PBS -M YOUREMAILHERE
+#PBS -N RunEviGene
+#PBS -l nodes=1:ppn=4,vmem=300gb,walltime=00:12:00:00
+
+module load evigene/gnu/2013.07.27
+module load blast 
+
+export BLASTDV=/N/soft/blastdb
+
+cd PWDHERE/final_assemblies/
+
+tr2aacds.pl -tidy -NCPU 3 -MAXMEM 131072 -log -cdna combined.fa
+
+/N/soft/rhel7/evigene/gnu/2013.07.27/scripts/prot/trclass2mainalt.pl -trclass *trclass
+mv *mainalt.tab annotation/
+cat okayset/combined.okay.fa okayset/combined.okalt.fa > annotation/transcripts.fa
+cat okayset/combined.okay.aa okayset/combined.okalt.aa > annotation/transcripts.aa
+
